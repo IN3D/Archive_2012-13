6e2d71399adaaaa525d78048315e4004dcf68b44
Daxter304 <daxter304@yahoo.com>
2013-09-12 05:25:42 -0400

    Many GUI Changes

--- Feudal/Form1.cs
+++ Feudal/Form1.cs
@@ -26,5 +26,10 @@ namespace Feudal
             aboutPage abtfrm = new aboutPage();
             abtfrm.Show();
         }
+
+        private void btnChangeGovt_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
