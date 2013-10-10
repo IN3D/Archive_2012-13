324db1e62ae231fd9d23d1d10dfd9348c4083a8a
IN3D <scarecrow3322@gmail.com>
2013-10-10 16:13:06 -0700

    Added Comments
    Added comments to the source code.

--- Source.cpp
+++ Source.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 using namespace std;
 
+//prototype
 void binary(int);
 
 int main()
 {
 	int number;
-
+	
+	//prompt the user for input
 	cout << "Please enter a positive integer ";
 	cin >> number;
-
+	
+	//validate the number
 	if (number < 0)
 		cout << "That is not a positive integer.";
 	else
 	{
 		cout << number << " converted to binary is ";
+		//call binary function
 		binary(number);
 		cout << endl;
 	}
@@ -34,5 +38,6 @@ void binary (int number)
 
 	remainder = number % 2;
 	binary(number >> 1);
+	//print out accumulated remainders
 	cout << remainder;
 }
