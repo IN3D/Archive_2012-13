297125af9bee3798938bb0a24ef392f7eeb755bb
IN3D <scarecrow3322@gmail.com>
2013-10-28 01:51:27 -0700

    Functional Build
    This is the first complete build of the game. The game was not initially
    performing correctly; the "human" element touching an "enemy" element
    did not end the game. Found a few strange lines of code while
    troubleshooting, and when compared against the book's source code, they
    weren't there; commenting them out resolved the issue.

--- SaveTheHumans/SaveTheHumans/MainPage.xaml.cs
+++ SaveTheHumans/SaveTheHumans/MainPage.xaml.cs
@@ -44,11 +44,23 @@ namespace SaveTheHumans
             if (progressBar.Value >= progressBar.Maximum)
                 EndTheGame();
         }
+        private void EndTheGame()
+        {
+            if (!playArea.Children.Contains(gameOverText))
+            {
+                enemyTimer.Stop();
+                targetTimer.Stop();
+                humanCaptured = false;
+                startButton.Visibility = Visibility.Visible;
+                playArea.Children.Add(gameOverText);
+            }
+        }
 
         private void enemyTimer_Tick(object sender, object e)
         {
-            throw new NotImplementedException();
+            AddEnemy();
         }
+
 
         /// <summary>
         /// Populates the page with content passed during navigation.  Any saved state is also
@@ -75,7 +87,20 @@ namespace SaveTheHumans
 
         private void startButton_Click(object sender, RoutedEventArgs e)
         {
-            AddEnemy();
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            human.IsHitTestVisible = true;
+            humanCaptured = false;
+            progressBar.Value = 0;
+            startButton.Visibility = Visibility.Collapsed;
+            playArea.Children.Clear();
+            playArea.Children.Add(target);
+            playArea.Children.Add(human);
+            enemyTimer.Start();
+            targetTimer.Start();
         }
 
         private void AddEnemy()
@@ -83,28 +108,88 @@ namespace SaveTheHumans
             ContentControl enemy = new ContentControl();
             enemy.Template = Resources["EnemyTemplate"] as ControlTemplate;
 
-
-
             AnimateEnemy(enemy, 0, playArea.ActualWidth - 100, "(Canvas.Left)");
             AnimateEnemy(enemy, rnd.Next((int) playArea.ActualHeight - 100),
                 rnd.Next((int) playArea.ActualHeight - 100), "(Canvas.Top)");
             playArea.Children.Add(enemy);
 
+            enemy.PointerEntered += enemy_PointerEntered;
+        }
+
+        void enemy_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            if (humanCaptured)
+                EndTheGame();
         }
 
         private void AnimateEnemy(ContentControl enemy, double from, double to, string propertyToAnimate)
         {
             Storyboard storyboard = new Storyboard() { AutoReverse = true, RepeatBehavior = RepeatBehavior.Forever };
-            DoubleAnimation animate = new DoubleAnimation()
+            DoubleAnimation animation = new DoubleAnimation()
             {
                 From = from,
                 To = to,
                 Duration = new Duration(TimeSpan.FromSeconds(rnd.Next(4, 6)))
             };
-            Storyboard.SetTarget(animate, enemy);
-            Storyboard.SetTargetProperty(animate, propertyToAnimate);
-            storyboard.Children.Add(animate);
+            Storyboard.SetTarget(animation, enemy);
+            Storyboard.SetTargetProperty(animation, propertyToAnimate);
+            storyboard.Children.Add(animation);
             storyboard.Begin();
         }
+
+        private void human_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (enemyTimer.IsEnabled)
+            {
+                humanCaptured = true;
+                human.IsHitTestVisible = false;
+            }
+        }
+
+        private void target_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            if (targetTimer.IsEnabled && humanCaptured)
+            {
+                progressBar.Value = 0;
+                Canvas.SetLeft(target, rnd.Next(100, (int)playArea.ActualWidth - 100));
+                Canvas.SetTop(target, rnd.Next(100, (int)playArea.ActualHeight - 100));
+                Canvas.SetLeft(human, rnd.Next(100, (int)playArea.ActualWidth - 100));
+                Canvas.SetTop(human, rnd.Next(100, (int)playArea.ActualHeight - 100));
+                /* The book says to add this code, but it's not in the source code
+                 * I think this is the code that is causing an issue with the game
+                 * not behaving properly (human touching enemy wasn't causing game over)
+                humanCaptured = false; //what? if humanCaptured, then !humanCaptured?
+                human.IsHitTestVisible = true;
+                */
+            }
+        }
+        private void playArea_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            if (humanCaptured)
+            {
+                Point pointerPosition = e.GetCurrentPoint(null).Position;
+                Point realativePosition = grid.TransformToVisual(playArea).TransformPoint(pointerPosition);
+                if ((Math.Abs(realativePosition.X - Canvas.GetLeft(human)) > human.ActualWidth * 3)
+                    || (Math.Abs(realativePosition.Y - Canvas.GetTop(human)) > human.ActualHeight * 3))
+                {
+                    humanCaptured = false;
+                    human.IsHitTestVisible = true;
+                }
+                else
+                {
+                    Canvas.SetLeft(human, realativePosition.X - human.ActualWidth / 2);
+                    Canvas.SetTop(human, realativePosition.Y - human.ActualHeight / 2);
+                }
+            }
+
+        }
+
+        private void playArea_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            if (humanCaptured)
+                EndTheGame();
+        }
+
+
     }
 }
