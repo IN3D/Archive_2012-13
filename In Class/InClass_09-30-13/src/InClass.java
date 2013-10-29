62c6d65875bfd38f478b348673eae2b4df46c6a2
Eric Hopkins <scarecrow3322@gmail.com>
2013-10-29 17:03:33 -0400

    Deleting the In Class folder
    Resoliving the issue to delete this In Class folder. The In Class
    folder is work that I had/have to follow along with while in the class
    room, I wasn't fond of how messy it was and it wasn't my work
    necessarily.

--- In Class/InClass_09-30-13/src/InClass.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * User: eric
- * Date: 30/09/13
- * Time: 6:11 PM
- * To change this template use File | Settings | File Templates.
- */
-public class InClass {
-}
