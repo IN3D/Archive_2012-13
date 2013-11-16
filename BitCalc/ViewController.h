a28a899750f1adcd84c45c067ee5921a9695d962
Eric Hopkins <scarecrow3322@gmail.com>
2013-11-16 12:29:23 -0500

    Initial Commit

---
+++ BitCalc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BitCalc
+//
+//  Created by Eric Hopkins on 11/16/13.
+//  Copyright (c) 2013 Eric Hopkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
