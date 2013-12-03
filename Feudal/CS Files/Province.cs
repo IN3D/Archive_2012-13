062c9a01556a6a4ca71ac4076d8eff6161c6a4cb
Eric Hopkins <scarecrow3322@gmail.com>
2013-12-03 18:00:14 -0500

    Breakup (It's not me, it's you)
    Broke up all the code that was sitting in Turns.cs and put them into
    relevant .cs files. So now turns, characters, and provinces are in
    seperate files instead of being one big mess.
    
    !!IMPORTANT!! I did this in SublimeText on my Mac, so it’s very
    possible I made little mistakes somewhere. I will check my work next
    time I’m in a windows environment with VS.

---
+++ Feudal/CS Files/Province.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace feudal
+{
+	public class province
+	{
+		private string provinceName;
+		//private color provinceColor;//not sure if color should be attached to a province or a person yet. TODO: Make decision
+		private int quality;
+		private double population;
+		private int stability;
+		//private int techMilitary;//implement tech later
+		//private int techEconomy;
+		//private int techGovernment;
+
+		//default constructor
+		//this proobably shouldn't be used, it's here just incase
+		public province()
+		{
+			this.provinceName = "Name Me!";
+			this.quality = 3;
+			this.population = 1400.00;
+			this.stability = 1;
+		}
+
+		public province(string provinceName, int quality, double population, int stability)
+		{
+			this.provinceName = provinceName;
+			this.quality = quality;
+			this.population = population;
+			this.stability = stability;
+		}
+
+		/**Accessors**/
+		public string getProvinceName()
+		{
+			return this.provinceName;
+		}
+
+		public int getQuality()
+		{
+			return this.quality;
+		}
+
+		public double getPopulation()
+		{
+			return this.population;
+		}
+
+		public int getStability()
+		{
+			return this.stability;
+		}
+
+		/**Modifiers**/
+		public void setProvinceName(string provinceName)
+		{
+			this.provinceName = provinceName;
+		}
+
+		public void setQuality(int quality)
+		{
+			this.quality = quality;
+		}
+
+		public void setPopulation(double population)
+		{
+			this.population = population;
+		}
+
+		public void setStability(int stability)
+		{
+			this.stability = stability;
+		}
+
+	}
+
+}
+			/******************************************************************
+			*Q: What the hell is this??										  *
+			*A: This was me working out exactly how characters should recieve *
+			* money based on province ownership. It's crap, but it's left here*
+			* for reference.												  *
+			* -Eric															  *
+			******************************************************************/
+            //!!!!!!!!!All of this should be moved!!!!!!!!!!!//
+            ////province is booming
+            //if (int_provinceQuality == 1)
+            //{
+            //    dbl_provIncome += 1.00;
+            //}
+            ////province is wealthy
+            //else if (int_provinceQuality == 2)
+            //{
+            //    dbl_provIncome += .75;
+            //}
+            ////province is average
+            //else if (int_provinceQuality == 3)
+            //{
+            //    dbl_provIncome += .50;
+            //}
+            ////province is poor
+            //else if (int_provinceQuality == 4)
+            //{
+            //    dbl_provIncome += .25;
+            //}
+            ////province is damaged
+            //else
+            //{
+            //    dbl_provIncome += 0.00;
+            //}
