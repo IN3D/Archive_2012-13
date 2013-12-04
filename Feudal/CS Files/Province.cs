b7d131c40ae42cc580fec9290521ee9dab9481f4
Eric Hopkins <scarecrow3322@gmail.com>
2013-12-04 14:41:47 -0500

    Files included, and formatting
    included characters.cs and province.cs to the Feudal.csproj. Also did
    some formatting on the comments so they weren’t running so long.

--- Feudal/CS Files/Province.cs
+++ Feudal/CS Files/Province.cs
@@ -8,7 +8,8 @@ namespace feudal
 	public class province
 	{
 		private string provinceName;
-		//private color provinceColor;//not sure if color should be attached to a province or a person yet. TODO: Make decision
+		//private color provinceColor;//not sure if color should be attached to a province or a person yet.
+		//TODO: Make decision
 		private int quality;
 		private double population;
 		private int stability;
