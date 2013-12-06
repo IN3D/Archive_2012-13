d3015c375f3cbc762a9bcde1c075353298991769
IN3D <scarecrow3322@gmail.com>
2013-12-06 01:42:10 -0800

    Religions Blueprint

--- Feudal/CS Files/ReligionTypes.cs
+++ Feudal/CS Files/ReligionTypes.cs
@@ -4,10 +4,36 @@ namespace Feudal
 {
 	public class ReligionTypes
 	{
-		//When declaring new Religions this is the format of the constructor
+		//See Religion_WriteUp in resources folder of dropbox for list of religions to implement.
+        //When declaring new Religions this is the format of the constructor
         //(Name of the religion, family of the religion, max zeal range, min zeal range, tolerance, bonusTax, bonusTrade, bonusStability, bonusTech)
+
+        //Christian Denominations//
         private Religion catholic = new Religion("Catholic", "Christian", 10, 1, 1.0, 0.5, -0.5, 0.35, -0.1);
         private Religion Protestant_Lib = new Religion("Liberal Protestant", "Christian", 6, 1, 1.0, 0.0, 0.25, 0.25, 0.0);
+        //TODO: Finish Christian denominations
+
+        //Islam Denominations//
+        //TODO: Finish Islam denominations
+
+        //Judaism//
+        //TODO: add Judaism
+
+        //Buddhism//
+        //TODO: add Buddhism denominations
+
+        //Indian Religions//
+        //TODO: add Indian religions
+
+        //Eastern Philosophy//
+        //TODO: add Eastern philosophies
+
+        //Pagan Religions//
+        //TODO: Add pagan religions
+
+        //Irreligion//
+        //TODO: Add irreligion
+
 
 		public ReligionTypes ()
 		{
