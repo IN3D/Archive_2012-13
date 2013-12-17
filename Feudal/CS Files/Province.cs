4257b0a1bc852312c48831f0ab019def5835ec96
Daxter304 <daxter304@yahoo.com>
2013-12-17 02:45:32 -0500

    Added econ, edited province.cs

--- Feudal/CS Files/Province.cs
+++ Feudal/CS Files/Province.cs
@@ -8,12 +8,13 @@ namespace Feudal
 	public class province
     {
         #region variables
-        private string provinceName;
+        private //can set private/public and all following until otherwise stated will be that
+        string provinceName;
 		//private color provinceColor;//not sure if color should be attached to a province or a person yet.
-		private int quality;
-		private int stability;
-		private double population;
-		private double wealth;
+		int quality;
+		int stability;
+		double population;
+		double wealth;
 		//private int techMilitary;//implement tech later
         //private int techEconomy;
 		//private int techGovernment;
---
+++ Feudal/CS Files/economy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Feudal.CS_Files
+{
+    public class economy : province
+    {
+        #region variables
+        private
+        double money;
+        double income;
+        double industry;
+        double commerce;
+        double taxes;
+        #endregion
+
+        #region constructor
+		public economy()
+        {
+            this.money = 1000000;
+            this.industry = 500;
+            this.commerce = 250;
+            //taxes based on quality/population
+            this.taxes = (Quality * Population);
+            //income should be the total of industry/commerce/taxes
+            this.income = (industry+commerce+taxes);
+        }
+	    #endregion
+        
+        
+
+    }
+}
