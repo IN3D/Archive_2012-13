b754ecd8be29500a5f75df87a9b1b94cae505f00
Eric Hopkins <scarecrow3322@gmail.com>
2013-12-19 19:55:47 -0500

    fixed up the abstract class, and a lot of formatting
    The way the abstract class ReligBonuses was declared  didn’t work how
    I’d envisioned. It required accessors to access from the subclass, now
    it just needs one set of accessors from the Religion subclass. Also
    there were too many tabs on all the lines, so I went through and fixed
    them.

--- Feudal/CS Files/Religion.cs
+++ Feudal/CS Files/Religion.cs
@@ -7,173 +7,163 @@ namespace Feudal
     public abstract class ReligBonuses
     {
         #region variables
-        private double bonusTax;
-        private double bonusTrade;
-        private double bonusStability;
-        private double bonusTech;
+		public double bonusTax;
+		public double bonusTrade;
+		public double bonusStability;
+		public double bonusTech;
         #endregion
-
-        #region accessors
-        public double BonusTax
-        {
-            get { return bonusTax; }
-            set { bonusTax = value; }
-        }
-        public double BonusTrade
-        {
-            get { return bonusTrade; }
-            set { bonusTrade = value; }
-        }
-        public double BonusStability
-        {
-            get { return bonusStability; }
-            set { bonusStability = value; }
-        }
-        public double BonusTech
-        {
-            get { return bonusTech; }
-            set { bonusTech = value; }
-
-        }
-        #endregion
-
     }
 
     public class Religion : ReligBonuses
     {
         #region variables
-            private string name;
-            private string family;
-            private int maxRange;//maybe not use these, but for now they
-            private int minRange;//were part of the original 
-            private double tolerance;
-            private object ReligBonuses;
-            private string fileLoc;
-            private Image icon;
-            //maybe make a bonuses class
-            //to have religion extend?
-            /*
-            private double bonusTax;
-            private double bonusTrade;
-            private double bonusStability;
-            private double bonusTech;
-            */
-            #endregion
+        private string name;
+        private string family;
+        private int maxRange;//maybe not use these, but for now they
+        private int minRange;//were part of the original 
+        private double tolerance;
+        private object ReligBonuses;
+        private string fileLoc;
+        private Image icon;
+        //maybe make a bonuses class
+        //to have religion extend?
+        /*
+        private double bonusTax;
+        private double bonusTrade;
+        private double bonusStability;
+        private double bonusTech;
+        */
+        #endregion
 
         #region constructors
-            //don't use default
-            public Religion()
-            {
-                name = "Not a religion";
-                family = "NULL";
-                maxRange = 10;
-                minRange = 1;
-                tolerance = 3.0;
-                BonusTax = 0.0;
-                BonusTrade = 0.0;
-                BonusTech = 0.0;
-                BonusStability = 0.0;
-                icon = Image.FromFile("..\\Resources\\religIcons\\Other\\Question_mark.png");
-                //var bonuses = ReligBonuses(0.0, 0.0, 0.0, 0.0) as ReligBonuses;
-            }
+        //don't use default
+        public Religion()
+        {
+            name = "Not a religion";
+            family = "NULL";
+            maxRange = 10;
+            minRange = 1;
+            tolerance = 3.0;
+			bonusTax = 0.0;
+			bonusTrade = 0.0;
+			bonusTech = 0.0;
+			bonusStability = 0.0;
+            icon = Image.FromFile("..\\Resources\\religIcons\\Other\\Question_mark.png");
+            //var bonuses = ReligBonuses(0.0, 0.0, 0.0, 0.0) as ReligBonuses;
+        }
 
-            //actually use this one
-            public Religion(string name, string family, int maxRange, int minRange,
-                           double tolerance, double bonusTax, double bonusTrade, double bonusStability,
-                           double bonusTech, string fileLoc)
+		//actually use this one
+        public Religion(string name, string family, int maxRange, int minRange,
+			double tolerance, double bonusTax, double bonusTrade, double bonusStability,
+			double bonusTech, string fileLoc)
+        {
+            this.name = name;
+			this.family = family;
+            this.maxRange = maxRange;
+            this.minRange = minRange;
+			this.tolerance = tolerance;
+            //ReligBonuses
+			this.bonusTrade = bonusTrade;
+			this.bonusTax = bonusTax;
+			this.bonusTech = bonusTech;
+			this.bonusStability = bonusStability;
+			//Image
+            try
             {
-                this.name = name;
-                this.family = family;
-                this.maxRange = maxRange;
-                this.minRange = minRange;
-                this.tolerance = tolerance;
-                //ReligBonuses
-                this.BonusTrade = bonusTrade;
-                this.BonusTax = bonusTax;
-                this.BonusTech = bonusTech;
-                this.BonusStability = bonusStability;
-                //Image
-                try
-                {
-                    this.Icon = Image.FromFile(fileLoc);
-                }
-                catch
-                {
-                    Console.WriteLine("Could not find " + fileLoc);
-                }
+				this.Icon = Image.FromFile(fileLoc);
             }
-            //deep copy constructor
-            public Religion(Religion e)
+            catch
             {
-                this.name = e.Name;
-                this.family = e.Family;
-                this.maxRange = e.MaxRange;
-                this.minRange = e.MinRange;
-                this.tolerance = e.Tolerance;
-                //ReligBonuses
-                this.BonusTax = e.BonusTax;
-                this.BonusTrade = e.BonusTrade;
-                this.BonusTech = e.BonusTech;
-                this.BonusStability = e.BonusStability;
+				Console.WriteLine("Could not find " + fileLoc);
             }
-            #endregion
+        }
+        //deep copy constructor
+		public Religion(Religion e)
+        {
+			this.name = e.Name;
+            this.family = e.Family;
+            this.maxRange = e.MaxRange;
+			this.minRange = e.MinRange;
+            this.tolerance = e.Tolerance;
+            //ReligBonuses
+			this.bonusTax = e.bonusTax;
+			this.bonusTrade = e.bonusTrade;
+			this.bonusTech = e.bonusTech;
+			this.bonusStability = e.bonusStability;
+		}
+        #endregion
 
-        #region accessors
-            public string Name
-            {
-                get { return name; }
-                set { name = value; }
-            }
+    	#region accessors
+		public string Name
+        {
+			get { return name; }
+            set { name = value; }
+        }
 
-            public string Family
-            {
-                get { return family; }
-                set { family = value; }
-            }
+        public string Family
+		{
+            get { return family; }
+			set { family = value; }
+        }
 
-            public int MaxRange
-            {
-                get { return maxRange; }
-                set { maxRange = value; }
-            }
+        public int MaxRange
+		{
+            get { return maxRange; }
+			set { maxRange = value; }
+        }
 
-            public int MinRange
-            {
-                get { return minRange; }
-                set { minRange = value; }
-            }
+        public int MinRange
+		{
+            get { return minRange; }
+			set { minRange = value; }
+        }
 
-            public double Tolerance
-            {
-                get { return tolerance; }
-                set { tolerance = value; }
+        public double Tolerance
+		{
+            get { return tolerance; }
+			set { tolerance = value; }
+        }
+        public Image Icon
+		{
+            get { return icon; }
+			set { icon = value; }
+        }
+        public string FileLoc
+		{
+            get { return fileLoc; }
+			set { fileLoc = value; }
+        }
+        public void ChangeIcon(Religion r)
+        {
+            try
+			{
+                this.Icon = Image.FromFile(r.fileLoc);
             }
-            public Image Icon//This doesn't seem to WAD
+			catch
             {
-                get { return icon; }
-                set { icon = value; }
-            }
-            public string FileLoc
-            {
-                get { return fileLoc; }
-                set { fileLoc = value; }
-            }
-            public void ChangeIcon(Religion r)
-            {
-                try
-                {
-                    this.Icon = Image.FromFile(r.fileLoc);
-                }
-                catch
-                {
-                    Console.WriteLine("Could not find object's " + r.fileLoc);
-                }
-            }
-            public double getBonusTax
-            {
-                get { return this.BonusTax; }
-                set { this.BonusTax = value; }
-            }
-            #endregion
+                Console.WriteLine("Could not find object's " + r.fileLoc);
+			}
+        }
+        public double BonusTax
+		{
+			get { return bonusTax; }
+			set { bonusTax = value; }
+        }
+		public double BonusTrade
+		{
+			get { return bonusTrade; }
+			set { bonusTrade = value; }
+		}
+		public double BonusStability
+		{
+			get { return bonusStability; }
+			set { bonusStability = value; }
+		}
+		public double BonusTech
+		{
+			get { return bonusTech; }
+			set { bonusTech = value; }
+		}
+		#endregion
     }
 }
