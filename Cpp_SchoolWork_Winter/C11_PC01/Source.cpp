0372d2fc93b333cdb76eddf0cfbb44aa672161e4
Eric Hopkins <scarecrow3322@gmail.com>
2014-01-17 19:22:56 -0500

    added input validation to source.cpp

--- Cpp_SchoolWork_Winter/C11_PC01/Source.cpp
+++ Cpp_SchoolWork_Winter/C11_PC01/Source.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <limits>
 #include "Source.h"
 
 using namespace std;
@@ -8,11 +9,20 @@ int main()
 {
     int tempNum = 0;
 	char again = 'y';
+
 	Number number(tempNum);
+
 	while (again == 'y' || again == 'Y')
 	{
 		cout << "Please enter a number between 0 and 9,999." << endl;
 		cin >> tempNum;
+        
+        while(cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "ERROR: invalid entry, please enter a number: ";
+            cin >> tempNum;
+        }
 
 		number.setNum(tempNum);
 
@@ -21,5 +31,6 @@ int main()
 		cout << "Go again? (y/n)" << endl;
 		cin >> again;
 	}
+    
 	return 0;
 }
