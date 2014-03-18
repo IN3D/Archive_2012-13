8bbc574361b8ef886dc974aa3ebbb7966223ef53
Eric Hopkins <scarecrow3322@gmail.com>
2014-03-17 20:47:20 -0400

    added TraitComparator.java

--- src/TraitComparator.java
+++ src/TraitComparator.java
@@ -10,25 +10,24 @@
  * by: Eric Hopkins
  *
  *
- * This file is used for the comparison of the skill linked list. This comparison uses the skill's names to alphabetize
+ * This file is used for the comparison of the trait linked list. This comparison uses the trait's names to alphabetize
  * them.
  */
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
 
-public class SkillComparator implements Comparator<Skill> {
+public class TraitComparator implements Comparator<Trait> {
 
     //NOTE: This should alphabetize names A-Z, if it is going Z-A turn the returns around.
     @Override
-    public int compare(Skill s1, Skill s2) {
+    public int compare(Trait t1, Trait t2) {
 
-        if (s1.getName().compareTo(s2.getName()) > 0) {
+        if (t1.getName().compareTo(t2.getName()) < 0) {
             return 1;
 
         } else {
             return -1;
-
         }
     }
 }
