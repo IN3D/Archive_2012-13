c2479acf19786ad3335e3e0fda3980cd9606868d
Eric Hopkins <scarecrow3322@gmail.com>
2014-03-24 20:41:10 -0700

    Successful test of reading xml file
    The skills xml file read in perfectly. Populating a linked list of
    skills with all of the skills listed in the xml file. It took in their
    names as well as the boolean value for weather or not they are
    difficult. While doing this I realized that I will need to adjust the
    way that the xml file for traits is set up so that it will be able to
    read into the program correctly.

--- src/Source.java
+++ src/Source.java
@@ -12,9 +12,71 @@
  *
  * This is the Source file of the program, the program runs through this file.
  */
+import java.util.*;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
 public class Source {
 
     public static void main(String args[]) {
+
+
+        //create a linked list to have the skills into
+        List<Skill> completeSkillsList = new LinkedList<Skill>();
+
+        //read in the xml file.
+        try {
+
+            File skillsFile = new File("./xml/skills.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document skillsDoc = dBuilder.parse(skillsFile);
+
+            skillsDoc.getDocumentElement().normalize();
+
+            System.out.println("XML parse test");
+            System.out.println("Root element: " + skillsDoc.getDocumentElement().getNodeName());
+
+            NodeList skillNodes = skillsDoc.getElementsByTagName("skill");
+
+            for (int i = 0; i < skillNodes.getLength(); i ++) {
+
+
+                Node currentNode = skillNodes.item(i);
+
+                if (currentNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element currentElement = (Element) currentNode;
+
+                    String tempName;
+                    boolean tempDifficult;
+
+                    tempName = currentElement.getElementsByTagName("name").item(0).getTextContent();
+
+                    if (currentElement.getElementsByTagName("difficult").item(0).getTextContent().equals("true")) {
+                        tempDifficult = true;
+                    } else {
+                        tempDifficult = false;
+                    }
+
+                    completeSkillsList.add(new Skill(tempDifficult, tempName));
+
+                }
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
 
         Character char1 = new Character("testCharacter", 24);
 
@@ -32,6 +94,11 @@ public class Source {
         char1.addToSkillsList(new Skill(false, "Zero-G Operations", ""));
 
         char1.printSkillsList();
+
+        for (Skill s:completeSkillsList) {
+
+            System.out.println("Name: " + s.getName()+ "\n Is difficult: " + s.getDifficult());
+        }
 
     }
 }
